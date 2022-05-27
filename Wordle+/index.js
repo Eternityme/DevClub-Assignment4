3c16b5cab397e12f892cd1ee9865c6e41afe0765
Eepsita <eepsita2304@gmail.com>
2022-05-27 15:37:48 +0530

    update index.js

--- Wordle+/index.js
+++ Wordle+/index.js
@@ -1,14 +1,29 @@
 const http = require("http");
+const url = require("url");
 
 const SECRET = "CIGAR"; // You can set any word as the secret answer
 
 function myFunction(req, res) {
-	// console.log({req}); // You can uncomment this to see the request object
+	console.log({req}); // You can uncomment this to see the request object
 	console.log(req.url);
-
-	const GUESS = ""; // Write logic to parse the word which the user guessed from the URL string
-	const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
-
+	//var GUESS = req.url; // Write logic to parse the word which the user guessed from the URL string
+	//GUESS = GUESS.slice(10)
+	//let length = GUESS.length
+	var guess = url.parse(req.url, true).query;
+	var feedback = "";
+	const GUESS = guess.q;
+	const len_g = GUESS.length;
+	for ( let x = 0 ; x < len_g ; x++ ){
+		if ((SECRET.includes(GUESS.charAt(x))) &&  (GUESS[x]==SECRET[x])){
+			feedback = feedback.concat("g");
+		}
+		else if ((SECRET.includes(GUESS.charAt(x))) &&  (GUESS[x]!=SECRET[x]))
+			feedback = feedback.concat("y");
+		else{
+			feedback = feedback.concat("b");
+		}
+	}
+	//const feedback = ""; // Write logic to compare the word with the secret, and generate the feedback string
 	res.write(feedback);
 	res.end();
 }
